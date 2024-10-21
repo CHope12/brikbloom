759453d0d49fd8a5ca5e116e14f9733a15cae340
jp010263 <c.hope@student.reading.ac.uk>
2024-10-21 14:48:13 +0100

    remove lenis

--- app/page.js
+++ app/page.js
@@ -1,6 +1,3 @@
-"use client";
-import { useEffect } from "react";
-
 import Nav from "@/components/Nav";
 import Hero from "@/components/Hero";
 import Services from "@/components/Services";
@@ -9,27 +6,8 @@ import Testimonials from "@/components/Testimonials";
 import How from "@/components/How";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
-import Lenis from "lenis";
 
 export default function Home() {
-
-  useEffect(() => {
-    // Initialize Lenis
-    const lenis = new Lenis();
-  
-    // Listen for the scroll event and log the event data
-    lenis.on('scroll', (e) => {
-      //console.log(e);
-    });
-  
-    // Use requestAnimationFrame to continuously update the scroll
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-  
-    requestAnimationFrame(raf);
-  }), [];
 
   return (
     <main className="overflow-hidden">
