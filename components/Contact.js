6d815a257e9f2ac1aed9648e5273ff698e2355a3
jp010263 <c.hope@student.reading.ac.uk>
2024-10-22 16:01:53 +0100

    contact form

--- components/Contact.js
+++ components/Contact.js
@@ -1,3 +1,7 @@
+const email = "team@brikbloom.com"
+//get access key from process.env
+const accessKey = process.env.ACCESS_KEY;
+
 const Contact = () => {
   return (
     <section id="contact" className="relative w-full h-[50rem] bg-white">
@@ -13,18 +17,19 @@ const Contact = () => {
       <div className="relative flex justify-center w-full">
       <div className="w-full max-w-7xl flex flex-col px-12 md:px-24 justify-center items-center gap-6">
         <h2 className="text-xl md:text-2xl text-gray-800">Ready to Bloom? Let's Talk!</h2>
-        <form action="" method="POST" className="flex flex-col w-full gap-4 text-gray-800">
-          <input type="text" placeholder="Name / Company" className="w-full border rounded-sm border-gray-200 px-4 py-2"/>
-          <input type="email" placeholder="Email*" className="w-full border rounded-sm border-gray-200 px-4 py-2"/>
-          <input type="tel" placeholder="Phone*" className="w-full border rounded-sm border-gray-200 px-4 py-2"/>
+        <form action="https://api.web3forms.com/submit" method="POST" className="flex flex-col w-full gap-4 text-gray-800">
+          <input type="hidden" name="access_key" value={accessKey} />
+          <input type="text" placeholder="Name / Company*" required className="w-full border rounded-sm border-gray-200 px-4 py-2"/>
+          <input type="email" placeholder="Email*" required className="w-full border rounded-sm border-gray-200 px-4 py-2"/>
+          <input type="tel" placeholder="Phone*" required className="w-full border rounded-sm border-gray-200 px-4 py-2"/>
           <textarea 
-            name="" 
-            id="" 
+            name="message"
             cols="30" 
             rows="10" 
             placeholder="Message"
             className="w-full border rounded-sm border-gray-200 px-4 py-2"
           />
+          <input type="checkbox" name="botcheck" className="hidden"/>
           <div className="flex justify-center items-center w-full">
             <button type="submit" className="bg-gray-300 hover:bg-gray-400 rounded-full w-32 py-4 font-bold">SEND</button>
           </div>
